19d12d266752a775152531158b5468a4f3b37af9
Astro <astro@spaceboyz.net>
2014-02-03 00:16:29 +0100

    functional spaceapi server, talks to HQSwitch

---
+++ spaceapi/HQSwitch.hs
@@ -0,0 +1,73 @@
+module HQSwitch where
+
+import Control.Monad
+import Data.Monoid
+import Data.String
+import System.ZMQ3.Monadic hiding (Off, On)
+import Control.Concurrent
+import qualified Data.ByteString as B
+import System.Time
+import Control.Concurrent.STM
+
+
+data State = Off
+           | On
+           | Full
+           | Error
+             deriving (Eq, Show)
+
+data Status = Status {
+      stState :: State,
+      stLastChange :: Integer
+    } deriving (Show)
+
+isOn :: Status -> Bool
+isOn status =
+    case stState status of
+      On -> True
+      Full -> True
+      _ -> False
+
+stMessage :: Status -> String
+stMessage status =
+    case stState status of
+      Off -> "GCHQ is off"
+      On -> "GCHQ is on"
+      Full -> "GCHQ is full"
+      _ -> "Error"
+
+run :: TVar Status -> IO ()
+run tStatus =
+    runZMQ $ do
+      req <- socket Req
+      connect req "tcp://beere.hq.c3d2.de:5555"
+      forever $ do
+        send req [] mempty
+        reply <- receive req
+
+        liftIO $ do
+          TOD now _ <- getClockTime
+          let state =
+                  case B.unpack reply of
+                    [0] -> Off
+                    [1] -> On
+                    [2] -> Full
+                    _ -> Error
+          changed <- atomically $ do
+            oldStatus <- readTVar tStatus
+            case stState oldStatus == state of
+              True ->
+                return False
+              False ->
+                do writeTVar tStatus $ Status state now
+                   return True
+          when (changed) $
+            putStrLn $ show now ++ " " ++ show state
+          threadDelay 1000000
+
+start :: IO (IO Status)
+start = do
+  TOD now _ <- getClockTime
+  tStatus <- newTVarIO $ Status Error now
+  _thr <- forkIO $ run tStatus
+  return $ readTVarIO tStatus
--- spaceapi/server.hs
+++ spaceapi/server.hs
@@ -3,14 +3,19 @@
 {-# LANGUAGE QuasiQuotes #-}
 {-# LANGUAGE TypeFamilies #-}
 
+import Data.Maybe
 import Control.Applicative
 import Yesod
 import Data.Aeson
+import Data.HashMap.Strict as HM
 import qualified Data.ByteString.Lazy.Char8 as LBC
 
+import qualified HQSwitch as Sw
 
+-- TODO: disable cookies, .cabal
 data App = App {
-      appJSON :: Object
+      appJSON :: Object,
+      appSwitch :: IO Sw.Status
     }
 
 mkYesod "App" [parseRoutes|
@@ -18,13 +23,28 @@ mkYesod "App" [parseRoutes|
 |]
 
 instance Yesod App where
+    makeSessionBackend _ = return Nothing
 
 getSpaceApiR :: Handler RepJson
 getSpaceApiR = do
-  App { appJSON = json } <- getYesod
-  return $ RepJson $ toContent $ Object json
+  App { appJSON = obj, appSwitch = sw } <- getYesod
+  swSt <- liftIO sw
+  let fromObj obj = case fromJSON obj of
+                      Success a -> Just a
+                      Error _ -> Nothing
+      stateObj = object $ [
+                  "open" .= Sw.isOn swSt,
+                  "message" .= Sw.stMessage swSt,
+                  "lastchange" .= Sw.stLastChange swSt
+                 ] ++ fromMaybe [] (HM.lookup "state" obj >>= fromObj)
+      obj' = HM.insert "state" stateObj obj
+  return $ RepJson $ toContent $ Object obj'
 
-
+main :: IO ()
 main = do
-  Just json <- decode <$> LBC.readFile "spaceapi.json"
-  warp 3000 $ App { appJSON = json }
+  app <- App <$>
+         fromMaybe (error "Cannot load spaceapi.json") <$>
+         decode <$>
+         LBC.readFile "spaceapi.json" <*>
+         Sw.start
+  warp 3000 app
