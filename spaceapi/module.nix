86ab7d54c7fa2be7d4390159176babc7659f50eb
Astro <astro@spaceboyz.net>
2019-04-08 03:05:53 +0200

    add module.nix for NixOS

---
+++ spaceapi/module.nix
@@ -0,0 +1,49 @@
+{ lib, pkgs, config, ... }:
+
+with lib;
+{
+  options.services.spaceapi = {
+    enable = mkOption {
+      type = types.bool;
+      default = true;
+      description = "Enable the SpaceAPI HTTP server";
+    };
+    spaceapiJson = mkOption {
+      type = types.path;
+      default = ./spaceapi.json;
+    };
+    wifiLocationsJson = mkOption {
+      type = types.path;
+      default = ./wifi-locations.json;
+    };
+  };
+
+  config =
+    let
+      cfg = config.services.spaceapi;
+      server = pkgs.callPackage ./default.nix {
+        inherit pkgs;
+        inherit (cfg) spaceapiJson wifiLocationsJson;
+      };
+    in mkIf cfg.enable {
+      systemd.services.spaceapi = {
+        description = "SpaceAPI server";
+        wantedBy = [ "multi-user.target" ];
+        after    = [ "network.target" ];
+        serviceConfig = {
+          Type = "simple";
+          ExecStart = "${server}/bin/spaceapi-server";
+          Restart = "always";
+          RestartSec = "10sec";
+
+          DynamicUser = true;
+          NoNewPrivileges = true;
+          LimitNPROC = 32;
+          LimitNOFILE = 256;
+          CPUWeight = 5;
+          MemoryMax = "512M";
+          ProtectSystem = "full";
+        };
+     };
+  };
+}
